df459bba2adcc5b32a99e80d4498f3e4be3b79be
Zowie Erickson <zowie.erickson@gmail.com>
2025-06-13 19:55:32 -0400

    chore: update API call to return limited number of objects

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -5,8 +5,13 @@ type Object = {
     objectIDs: number[]
 }
 
+type Item = {
+    objectIDs: number[]
+}
+
 export default function Gallery() {
     const [data, setData] = useState({})
+    const [items, setItems] = useState<Item[]>([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState<string | null>(null)
 
@@ -14,14 +19,16 @@ export default function Gallery() {
         const fetchArt = async () => {
             try {
                 setLoading(true)
-                const response = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects')
+                const response = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects?limit=0')
 
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`)
                 }
 
                 const data : any = await response.json()
-                setData(data)
+                
+                setItems(data.objectIDs)
+                setData(data.objectIDs.slice(0, 20))
             } catch (err) {
                 setError("idk")
             } finally {
@@ -34,6 +41,11 @@ export default function Gallery() {
 
     console.log(data)
     return (
-        <h1>Hello, Gallery!</h1>
+        <section>
+            <h1>Hello, Gallery!~!</h1>
+            {items.map(itemDuh => {
+                return <p>Ok</p>
+            })}
+        </section>
     )
 }
